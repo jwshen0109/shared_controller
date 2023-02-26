dbec32b977f57419021c805d7940d83c0efc7412
jwshen0109 <jwshen0109@qq.com>
2023-02-26 14:52:50 +0800

    remove sword

--- sword/scripts/backend.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-import sys
-sys.path.append("/home/ur5e/zh/ACORN/experiment_scripts/")
-import ros_infer
-
-if __name__ == "__main__":
-    # ros_infer.ros_serve()
-    ros_infer.ros_serve_penalty()
--- sword/scripts/env_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-import torch
-
-
-if __name__ == '__main__':
-    print(torch.cuda.is_available())
